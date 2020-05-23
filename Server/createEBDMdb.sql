7cf42a328d0eeec29c1d9cdfc5465c1ada177281
Steven Balagtas <scbalagtas@gmail.com>
2020-05-23 18:07:43 +1000

    fixed typo inside the createEBDMdb script

--- Server/createEBDMdb.sql
+++ Server/createEBDMdb.sql
@@ -1,9 +1,9 @@
 /*
  *  Author: Steven Balagtas
- *  This script will create the EBDM database inside you're mysql/ mariadb database server.
+ *  This script will create the EBDM database inside your mysql/ mariadb database server.
  *
  *  To execute use the following command in your command line: "mysql -u root -p < createEBDMdb.sql"
- *  Or if executing from within the mysql command line use: "source 'path to the script'"
+ *  Or if executing from within the mysql command line use: "source 'full path to the script'"
  */
 
 CREATE DATABASE IF NOT EXISTS EBDM;
