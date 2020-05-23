f8e06e550725b9b4389942238d52ac08770e2a59
Steven Balagtas <scbalagtas@gmail.com>
2020-05-23 17:22:11 +1000

    created DBConnection class

---
+++ Server/src/Database/DBConnection.java
@@ -0,0 +1,59 @@
+package Database;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Change the db.props file to change which database to connect to.
+ */
+
+public class DBConnection {
+    /**
+     * The singleton instance of the database connection.
+     */
+    private static Connection connection = null;
+
+    /**
+     * Constructor intialises the connection.
+     */
+    private DBConnection() {
+        Properties props = new Properties();
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("./db.props");
+            props.load(in);
+            in.close();
+
+            // specify the data source, username and password
+            String url = props.getProperty("jdbc.url");
+            String username = props.getProperty("jdbc.username");
+            String password = props.getProperty("jdbc.password");
+            String schema = props.getProperty("jdbc.schema");
+
+            // get a connection
+            connection = DriverManager.getConnection(url + "/" + schema,
+                    username, password);
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println(fnfe);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Provides global access to the singleton instance of the UrlSet.
+     *
+     * @return a handle to the singleton instance of the UrlSet.
+     */
+    public static Connection getConnection() {
+        if (connection == null) { new DBConnection(); }
+        return connection;
+    }
+}
