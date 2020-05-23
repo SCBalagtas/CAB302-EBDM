960ae60ecf52f0bec52da35f8d339e8aa6bfc026
Steven Balagtas <scbalagtas@gmail.com>
2020-05-23 15:57:38 +1000

    created db.props file and createEBDMdb script

---
+++ Server/createEBDMdb.sql
@@ -0,0 +1,8 @@
+/*
+ *  Author: Steven Balagtas
+ *  This script will create the EBDM database inside you're mysql/ mariadb database server.
+ *
+ *  To execute use the following command: "mysql -u root -p < createEBDMdb.sql"
+ */
+
+CREATE DATABASE IF NOT EXISTS EBDM;
