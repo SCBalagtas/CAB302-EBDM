e3ea81533f4770dd04c52b4863e414fee90c06fd
Charles Cruz <n9828940@connect.qut.edu.au>
2020-05-29 16:12:54 +1000

    createbillboard gui can now change text colours for billboard message and information

--- Control_Panel/src/CreateBillboard.java
+++ Control_Panel/src/CreateBillboard.java
@@ -11,6 +11,7 @@ public class CreateBillboard extends JFrame implements Runnable {
     private String BBImgUrl;
     private String BBImgData;
     private String BBInfo;
+    private String BBPicMode;
 
     public CreateBillboard(String title) throws HeadlessException {
         super(title);
@@ -33,14 +34,14 @@ public class CreateBillboard extends JFrame implements Runnable {
         submitBtnPanel.setLayout(new GridBagLayout());
 
         previewPanel.setBackground(Color.BLUE);
-        mainJPanel.setBackground(Color.RED);
         submitBtnPanel.setBackground(Color.GREEN);
 
         getContentPane().add(previewPanel, BorderLayout.NORTH);
-        getContentPane().add(mainJPanel, BorderLayout.CENTER);
+        getContentPane().add(mainJPanel, BorderLayout.WEST);
         getContentPane().add(submitBtnPanel, BorderLayout.SOUTH);
 
         GridBagConstraints gbCons = new GridBagConstraints();
+        gbCons.anchor = GridBagConstraints.LINE_START;
 
         // Preview Panel Widgets:
         JLabel bbPreviewLbl = new JLabel("Preview:");
@@ -52,26 +53,22 @@ public class CreateBillboard extends JFrame implements Runnable {
         // Main Panel Widgets:
         // Billboard Author
         JLabel bbAuthorLbl = new JLabel("Author:");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 0;
         mainJPanel.add(bbAuthorLbl, gbCons);
 
         JLabel bbAuthor = new JLabel("Test Author");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 0;
         mainJPanel.add(bbAuthor, gbCons);
 
         // Import and Export Buttons
         JButton bbImportBtn = new JButton("Import");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 1;
         mainJPanel.add(bbImportBtn, gbCons);
 
         JButton bbExportBtn = new JButton("Export");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 1;
         mainJPanel.add(bbExportBtn, gbCons);
@@ -79,14 +76,12 @@ public class CreateBillboard extends JFrame implements Runnable {
         // Billboard Name
         // label
         JLabel bbNameLbl = new JLabel("Name:");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 2;
         mainJPanel.add(bbNameLbl, gbCons);
 
         // text field
         JTextField bbNameTf = new JTextField(20);
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 2;
         mainJPanel.add(bbNameTf, gbCons);
@@ -94,52 +89,148 @@ public class CreateBillboard extends JFrame implements Runnable {
         // Billboard Message
         // label
         JLabel bbMsgLbl = new JLabel("Message:");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 3;
         mainJPanel.add(bbMsgLbl, gbCons);
 
         // text field
         JTextField bbMsgTf = new JTextField(20);
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 3;
         mainJPanel.add(bbMsgTf, gbCons);
 
-        // Billboard Picture (URL)
-        // label
-        JLabel bbPicUrlLbl = new JLabel("Image Url:");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
-        gbCons.gridx = 0;
-        gbCons.gridy = 4;
-        mainJPanel.add(bbPicUrlLbl, gbCons);
+        JButton bbMsgCc = new JButton("Change Colour");
+        gbCons.fill = GridBagConstraints.NONE;
+        gbCons.gridx = 2;
+        gbCons.gridy = 3;
+        mainJPanel.add(bbMsgCc, gbCons);
 
-        // text field
-        JTextField bbPicUrlTf = new JTextField(20);
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
+        bbMsgCc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color color = JColorChooser.showDialog(new JFrame(),"Change Message Colour", Color.WHITE);
+
+                if (color != null) {
+                    System.out.println(String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue()));
+                }
+
+            }
+        });
+
+        // Billboard Picture
+        BBPicMode = "url"; // url mode is the initial value when uploading images
+
+        JPanel bbPicUrlPanel = new JPanel();
+        bbPicUrlPanel.setLayout(new GridBagLayout());
+        bbPicUrlPanel.setBackground(Color.GREEN);
+        gbCons.gridx = 2;
+        gbCons.gridy = 4;
+        mainJPanel.add(bbPicUrlPanel, gbCons);
+
+        // URL label & text field
+        JLabel bbPicUrlLbl = new JLabel("Image URL:");
+        gbCons.gridx = 0;
+        gbCons.gridy = 0;
+        bbPicUrlPanel.add(bbPicUrlLbl, gbCons);
+
+        JTextField bbPicUrlTf = new JTextField("", 40);
+        gbCons.fill = GridBagConstraints.BOTH;
+        gbCons.gridx = 1;
+        gbCons.gridy = 0;
+        bbPicUrlPanel.add(bbPicUrlTf, gbCons);
+
+
+        JPanel bbPicDataPanel = new JPanel();
+        bbPicDataPanel.setLayout(new GridBagLayout());
+        bbPicDataPanel.setBackground(Color.GREEN);
+        gbCons.gridx = 2;
+        gbCons.gridy = 4;
+        mainJPanel.add(bbPicDataPanel, gbCons);
+        bbPicDataPanel.setVisible(false);
+
+        JLabel bbUploadLbl = new JLabel("Image Location...");
+        gbCons.gridx = 0;
+        gbCons.gridy = 0;
+        bbPicDataPanel.add(bbUploadLbl, gbCons);
+
+        JButton bbUploadBtn = new JButton("Upload");
+        gbCons.gridx = 1;
+        gbCons.gridy = 0;
+        bbPicDataPanel.add(bbUploadBtn, gbCons);
+
+        // bbPic Options
+        JPanel bbPicPanel = new JPanel();
+        bbPicPanel.setLayout(new GridLayout(1,0));
         gbCons.gridx = 1;
         gbCons.gridy = 4;
-        mainJPanel.add(bbPicUrlTf, gbCons);
+        mainJPanel.add(bbPicPanel, gbCons);
 
-        // Billboard Picture (URL)
+        JRadioButton bbPicUrlRb = new JRadioButton("URL");
+        bbPicUrlRb.setSelected(true);
+        bbPicUrlRb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BBPicMode = "url";
+                bbPicUrlPanel.setVisible(true);
+                bbPicDataPanel.setVisible(false);
+
+            }
+        });
+
+
+        JRadioButton bbPicDataRb = new JRadioButton("Upload");
+        bbPicDataRb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BBPicMode = "data";
+                bbPicUrlPanel.setVisible(false);
+                bbPicDataPanel.setVisible(true);
+            }
+        });
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(bbPicUrlRb);
+        group.add(bbPicDataRb);
+
+        bbPicPanel.add(bbPicUrlRb);
+        bbPicPanel.add(bbPicDataRb);
+
+
+        // Billboard Information
         // label
         JLabel bbInfoLbl = new JLabel("Information:");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 5;
         mainJPanel.add(bbInfoLbl, gbCons);
 
         // text field
         JTextField bbInfoTf = new JTextField(20);
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 5;
         mainJPanel.add(bbInfoTf, gbCons);
 
+        JButton bbInfoCc = new JButton("Change Colour");
+        gbCons.fill = GridBagConstraints.NONE;
+        gbCons.gridx = 2;
+        gbCons.gridy = 5;
+        mainJPanel.add(bbInfoCc, gbCons);
+
+        bbInfoCc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color color = JColorChooser.showDialog(new JFrame(),"Change Information Colour", Color.WHITE);
+
+                if (color != null) {
+                    System.out.println(String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue()));
+                }
+
+            }
+        });
+
+
         // Submit Panel Widgets:
         // Create and Cancel Buttons
         JButton bbCancelBtn = new JButton("Cancel");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 0;
         gbCons.gridy = 0;
         submitBtnPanel.add(bbCancelBtn, gbCons);
@@ -148,14 +239,11 @@ public class CreateBillboard extends JFrame implements Runnable {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Object source = e.getSource();
-                JButton sourceJButton = (JButton) source;
-                System.out.println(sourceJButton.getText());
                 dispose();
             }
         });
 
         JButton bbCreateBtn = new JButton("Create Billboard");
-        gbCons.fill = GridBagConstraints.HORIZONTAL;
         gbCons.gridx = 1;
         gbCons.gridy = 0;
         submitBtnPanel.add(bbCreateBtn, gbCons);
@@ -164,8 +252,10 @@ public class CreateBillboard extends JFrame implements Runnable {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Object source = e.getSource();
-                JButton sourceJButton = (JButton) source;
-                System.out.println(sourceJButton.getText());
+
+                // validate fields
+
+                // exit window
                 dispose();
             }
         });
